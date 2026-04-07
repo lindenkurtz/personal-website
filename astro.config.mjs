0c5953152c661f2575a06a486485f7aabb4945ff
Linden Kurtz <liku5925@colorado.edu>
2026-04-07 09:21:54 -0600

    fixed api rendering and added resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  adapter: process.env.NODE_ENV === 'production' ? cloudflare() : undefined,
   vite: {
     plugins: [tailwindcss()],
-  },
+  },  
 });
